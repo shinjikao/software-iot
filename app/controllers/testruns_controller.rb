febe3bc81ea62ea5e4219c73ecfeaa5d828c04c9
shinjikao <shinjikao@gmail.com>
2015-03-09 00:22:53 +0800

    fix json format
    fix json format

--- app/controllers/testruns_controller.rb
+++ app/controllers/testruns_controller.rb
@@ -5,41 +5,45 @@ class TestrunsController < ApplicationController
 	end
 
 	def new
+		
 		testrun_hash=params
-
+		
 
 		#file = File.read('report.json')
-	   # testrun_hash = JSON.parse(file)
-	    #@testrun_hash = testrun_hash
-
-
+	    #@testrun_hash = JSON.parse(file)
+	    
+	    
+		
+	    
 		tr = Testrun.new
-
 		tr.TestRun_Time = params['TestRun_Time']
 		tr.TestRun_Path = params['TestRun_Path']
 		tr.Product      = params['Product']
 		tr.SerialNumber = params['SerialNumber']
 		tr.DeviceVersion = params['DeviceVersion']
 		tr.AndroidVersion = params['AndroidVersion']
+		params['TestRun_Apps'].each do | app|
+	    	
+			ap = App.new
+			ap.AppNumber = app['AppNumber']
+			ap.AppName = app['AppName']
+			ap.AppVersion =app['AppVersion']
+			ap.AppSKU = app['AppSKU']
+			ap.AppClass = app['AppClass']
+			ap.AppCategory = app['AppCategory']
+			ap.AppPackageName = app['AppPackageName']
+			ap.ApkStatus = app['ApkStatus']
+			ap.ApkResult = app['ApkResult']
+			ap.AppInstallResult = app['AppInstallResult']
+			ap.AppLaunchResult = app['AppLaunchResult']
+			ap.testrun = tr
+			ap.save	
+			end
+			
+			tr.save
 		
-
-		ap = App.new
-		ap.AppNumber = params['TestRun_Apps']['AppNumber']
-		ap.AppName = params['TestRun_Apps']['AppName']
-		ap.AppVersion = params['TestRun_Apps']['AppVersion']
-		ap.AppSKU = params['TestRun_Apps']['AppSKU']
-		ap.AppClass = params['TestRun_Apps']['AppClass']
-		ap.AppCategory = params['TestRun_Apps']['AppCategory']
-		ap.AppPackageName = params['TestRun_Apps']['AppPackageName']
-		ap.ApkStatus = params['TestRun_Apps']['ApkStatus']
-		ap.ApkResult = params['TestRun_Apps']['ApkResult']
-		ap.AppInstallResult = params['TestRun_Apps']['AppInstallResult']
-		ap.AppLaunchResult = params['TestRun_Apps']['AppLaunchResult']
-		ap.testrun = tr
-		ap.save	
-    	tr.save
-      
-       redirect_to testruns_url
+      	
+    	#redirect_to testruns_url
 	end
 
 	def create
