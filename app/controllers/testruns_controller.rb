e287ac1370b0ce694a02703edf368fc54427a059
shinjikao <shinjikao@gmail.com>
2015-03-09 18:29:58 +0800

    is_a function test
    function test

--- app/controllers/testruns_controller.rb
+++ app/controllers/testruns_controller.rb
@@ -9,6 +9,12 @@ class TestrunsController < ApplicationController
 		#testrun_hash=params
 		@json_obj=params
 
+		@isAString = params.is_a? String
+		@isAHash = params.is_a? Hash
+		@isAJSON = params.is_a? JSON
+		
+
+
 		file = File.read('report.json')
 	    testrun_hash = JSON.parse(file)
 	    
@@ -48,7 +54,45 @@ class TestrunsController < ApplicationController
 	end
 
 	def create
+	    #testrun_hash=params
+		@json_obj2=params
+
+		file = File.read('report.json')
+	    testrun_hash = JSON.parse(file)
 	    
+	    
+		testrun_hash.each do |hash|
+	    #params.each do |hash|
+		tr = Testrun.new
+		
+		tr.TestRun_Time = hash['TestRun_Time']
+		tr.TestRun_Path = hash['TestRun_Path']
+		tr.Product      = hash['Product']
+		tr.SerialNumber = hash['SerialNumber']
+		tr.DeviceVersion = hash['DeviceVersion']
+		tr.AndroidVersion = hash['AndroidVersion']
+		hash['TestRun_Apps'].each do | app|
+	    	
+			ap = App.new
+			ap.AppNumber = app['AppNumber']
+			ap.AppName = app['AppName']
+			ap.AppVersion =app['AppVersion']
+			ap.AppSKU = app['AppSKU']
+			ap.AppClass = app['AppClass']
+			ap.AppCategory = app['AppCategory']
+			ap.AppPackageName = app['AppPackageName']
+			ap.ApkStatus = app['ApkStatus']
+			ap.ApkResult = app['ApkResult']
+			ap.AppInstallResult = app['AppInstallResult']
+			ap.AppLaunchResult = app['AppLaunchResult']
+			ap.testrun = tr
+			ap.save	
+			end
+			
+			tr.save
+		end
+      	
+    	#redirect_to testruns_url
 	end
 
 
