e98a49254f34e0de9684f8457525688b49991649
shinjikao <shinjikao@gmail.com>
2015-03-10 18:51:11 +0800

    sad
    sad

--- app/controllers/testruns_controller.rb
+++ app/controllers/testruns_controller.rb
@@ -34,43 +34,12 @@ class TestrunsController < ApplicationController
 	end
 
 	def create
-	    #testrun_hash=params
-		@json_obj2=params
 
-		file = File.read('report.json')
-	    testrun_hash = JSON.parse(file)
-	    
-	    
-		testrun_hash.each do |hash|
-	    #params.each do |hash|
-		tr = Testrun.new
-		
-		tr.TestRun_Time = hash['TestRun_Time']
-		tr.TestRun_Path = hash['TestRun_Path']
-		tr.Product      = hash['Product']
-		tr.SerialNumber = hash['SerialNumber']
-		tr.DeviceVersion = hash['DeviceVersion']
-		tr.AndroidVersion = hash['AndroidVersion']
-		hash['TestRun_Apps'].each do | app|
-	    	
-			ap = App.new
-			ap.AppNumber = app['AppNumber']
-			ap.AppName = app['AppName']
-			ap.AppVersion =app['AppVersion']
-			ap.AppSKU = app['AppSKU']
-			ap.AppClass = app['AppClass']
-			ap.AppCategory = app['AppCategory']
-			ap.AppPackageName = app['AppPackageName']
-			ap.ApkStatus = app['ApkStatus']
-			ap.ApkResult = app['ApkResult']
-			ap.AppInstallResult = app['AppInstallResult']
-			ap.AppLaunchResult = app['AppLaunchResult']
-			ap.testrun = tr
-			ap.save	
-			end
-			
-			tr.save
-		end
+		@pa = params
+	    @h = params.to_h
+	    @a = params.to_a
+		@s = params.to_s
+		@pa_inspect = params.inspect
       	
     	#redirect_to testruns_url
 	end
