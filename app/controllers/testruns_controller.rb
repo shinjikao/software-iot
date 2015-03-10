a6ceeebbdd0d62e4960aa02000eeadc25cb71ba1
shinjikao <shinjikao@gmail.com>
2015-03-10 16:01:16 +0800

    hhh
    hhh

--- app/controllers/testruns_controller.rb
+++ app/controllers/testruns_controller.rb
@@ -7,48 +7,43 @@ class TestrunsController < ApplicationController
 	def new
 		
 		#testrun_hash=params
-		@params_class=params.class
+		testrun_hash = JSON.parse(params)
+		@hh = JSON.parse(params)
+
+		#file = File.read('report0310.json')
+	    #testrun_hash = JSON.parse(file)
+
+	    tr = Testrun.new
+			
+		tr.TestRun_Time = testrun_hash['TestRun_Time']
+		tr.Product      = testrun_hash['Product']
+		tr.SerialNumber = testrun_hash['SerialNumber']
+		tr.DeviceVersion = testrun_hash['DeviceVersion']
+		tr.AndroidVersion = testrun_hash['AndroidVersion']
+		
+		testrun_hash['TestRun_Apps'].each do |app|			
+			ap = App.new
+			ap.AppNumber = app['AppNumber']
+			ap.AppName = app['AppName']
+			ap.AppVersion =app['AppVersion']
+			ap.AppSKU = app['AppSKU']
+		    ap.AppClass =app['AppClass']
+		    ap.AppCategory =app['AppCategory']
+		    ap.AppPackageName = app['AppPackageName']
+		    ap.ApkStatus = app['ApkStatus']
+		    ap.ApkResult = app['ApkResult']
+		    ap.AppInstallResult = app['AppInstallResult']
+		    ap.AppLaunchResult =app['AppLaunchResult']
+			ap.testrun = tr
+			ap.save	
+			end
 
 		
-
-		file = File.read('report.json')
-	    testrun_hash = JSON.parse(file)
-	    
-	    
-		testrun_hash.each do |hash|
-	    #params.each do |hash|
-		tr = Testrun.new
+			
 		
-		tr.TestRun_Time = hash['TestRun_Time']
-		tr.TestRun_Path = hash['TestRun_Path']
-		tr.Product      = hash['Product']
-		tr.SerialNumber = hash['SerialNumber']
-		tr.DeviceVersion = hash['DeviceVersion']
-		tr.AndroidVersion = hash['AndroidVersion']
-		
-
 	
-
-		#hash['TestRun_Apps'].each do | app|
-	    	
-		#	ap = App.new
-		#	ap.AppNumber = app['AppNumber']
-		#	ap.AppName = app['AppName']
-		#	ap.AppVersion =app['AppVersion']
-		#	ap.AppSKU = app['AppSKU']
-		#	ap.AppClass = app['AppClass']
-		#	ap.AppCategory = app['AppCategory']
-		#	ap.AppPackageName = app['AppPackageName']
-		#	ap.ApkStatus = app['ApkStatus']
-		#	ap.ApkResult = app['ApkResult']
-		#	ap.AppInstallResult = app['AppInstallResult']
-		#	ap.AppLaunchResult = app['AppLaunchResult']
-		#	ap.testrun = tr
-		#	ap.save	
-		#	end
-		
 			tr.save
-		end
+		
       	
     	#redirect_to testruns_url
 	end
