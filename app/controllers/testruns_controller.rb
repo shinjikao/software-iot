5e330a1833ee087479de848b1fcd2e521d5fb488
shinjikao <shinjikao@gmail.com>
2015-03-10 20:20:05 +0800

    sda
    asd

--- app/controllers/testruns_controller.rb
+++ app/controllers/testruns_controller.rb
@@ -13,21 +13,16 @@ class TestrunsController < ApplicationController
 		#file = File.read('report0310.json')
 		tr = Testrun.new
 		tr.TestRun_Time =params[:TestRun_Time]
+		tr.TestRun_Time = params[:TestRun_Time]
+		tr.Product      = params[:Product]
+		tr.SerialNumber = params[:SerialNumber]
+		tr.DeviceVersion = params[:DeviceVersion]
+		tr.AndroidVersion = params[:AndroidVersion]
 	    tr.save
-		#tr= Testrun.new
-		#tr.TestRun_Time = hash[:TestRun_Time]
-		#tr.save
-
-		#params.collect { |k,v| "#{k} ,#{v}" }
-	    #tr = Testrun.new
-		#tr.TestRun_Time = testrun_hash[:TestRun_Time]
-		#tr.Product      = testrun_hash[:Product]
-		#tr.SerialNumber = testrun_hash[:SerialNumber]
-		#tr.DeviceVersion = testrun_hash[:DeviceVersion]
-		#tr.AndroidVersion = testrun_hash[:AndroidVersion]
-		#tr.save
+		
+		
 		#@t = tr 
-    	#redirect_to testruns_url
+    	redirect_to testruns_url
 	end
 
 	def create
